c7fc0aeeea4d01d599b5490753c6735b87a31b7c
justbert <justin.m.bertrand@gmail.com>
2016-09-30 13:53:28 -0400

    Beginning of the project. Setting up basic shapes.

--- Template/main.cpp
+++ Template/main.cpp
@@ -1,8 +1,30 @@
 #include <SFML/Graphics.hpp>
+#include "TransformNode.h"
 
 int main(int argc, char *argv)
 {
-  sf::RenderWindow window(sf::VideoMode(400, 400), "Template", sf::Style::Titlebar | sf::Style::Close);
+	//Initial Values
+	sf::Vector2<float> bodySize(100.0f, 400.0f);
+	sf::Vector2<float> armSize(20.0f, 50.0f);
+	sf::Vector2<float> neckSize(50.0f, 25.0f);
+	float headSize = 75.0f;
+	std::size_t headPoints = 60;
+
+	//Create Window	
+  sf::RenderWindow window(sf::VideoMode(600, 600), "WackyInflatableMan", sf::Style::Titlebar | sf::Style::Close);
+
+  //create shapes
+  sf::RectangleShape bodyRect(bodySize);
+  sf::RectangleShape leftUpperArmRect(armSize);
+  sf::RectangleShape leftLowerArmRect(armSize);
+  sf::RectangleShape rightUpperArmRect(armSize);
+  sf::RectangleShape rightLowerArmRect(armSize);
+  sf::RectangleShape neckRect(neckSize);
+  sf::CircleShape headCircle(headSize, headPoints);
+
+  //Set body's properties
+  bodyRect.setPosition(window.getSize().x / 2, 0);
+  bodyRect.setOrigin(bodyRect.getSize().x / 2, 0);
 
   sf::Event evt;
   sf::Clock appTimer;
